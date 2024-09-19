c505c4e707b428bd9b425942237380e56700d1d2
Oksana <vyshynska2007@gmail.com>
2024-09-19 08:27:53 +0300

    add new file

--- Lab_02.1.cpp
+++ Lab_02.1.cpp
@@ -14,17 +14,17 @@ int main()
     double Pi = 4 * atan(1.);
 
     double a;
-    double z1;
-    //double z2;
+    //double z1;
+    double z2;
 
     cout << "a= "; cin>>a;
 
-    z1=2*( sin((3*4*atan(1.))-2*a) * sin((3*4*atan(1.))-2*a) ) * ( cos((5*4*atan(1.))+2*a) * cos ((5*4*atan(1.))+2*a) );
-    //z2=1.0/4 - 1.0/4 * sin(5.0/2*(4*atan(1.0))-8*a);
+    //z1=2*( sin((3*4*atan(1.))-2*a) * sin((3*4*atan(1.))-2*a) ) * ( cos((5*4*atan(1.))+2*a) * cos ((5*4*atan(1.))+2*a) );
+    z2=1.0/4 - 1.0/4 * sin(5.0/2*(4*atan(1.0))-8*a);
 
     cout<<endl;
-    cout<<"z1= "<<z1<<endl;
-    //cout<<"z2= "<<z2<<endl;
+    //cout<<"z1= "<<z1<<endl;
+    cout<<"z2= "<<z2<<endl;
 
     cin.get();
     return 0;
